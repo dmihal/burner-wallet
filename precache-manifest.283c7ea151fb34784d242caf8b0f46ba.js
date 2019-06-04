3ef1db4886a061d986ddc1e6a947fac774c5bb50
David Mihal <david@dmihal.com>
2019-06-04 12:06:54 +0300

    Updates

---
+++ precache-manifest.283c7ea151fb34784d242caf8b0f46ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb8383bf2ba6c8fa686a4c9a33baf00",
+    "url": "/burner-wallet/index.html"
+  },
+  {
+    "revision": "d7145740a11608324126",
+    "url": "/burner-wallet/static/css/main.ba7977dd.chunk.css"
+  },
+  {
+    "revision": "99f415b080613cfd1e54",
+    "url": "/burner-wallet/static/js/2.29c02940.chunk.js"
+  },
+  {
+    "revision": "d7145740a11608324126",
+    "url": "/burner-wallet/static/js/main.d274767a.chunk.js"
+  },
+  {
+    "revision": "7d89c980a6ae3f42a1a7",
+    "url": "/burner-wallet/static/js/runtime~main.72a26835.js"
+  }
+]);
